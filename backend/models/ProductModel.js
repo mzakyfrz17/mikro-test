2946ef748db96ed5e658f88f92b28666c585e576
mzakyfrz17 <mzakyfrz17@gmail.com>
2023-12-13 09:28:24 +0700

    first commit

---
+++ backend/models/ProductModel.js
@@ -0,0 +1,21 @@
+import {Sequelize} from "sequelize";
+import db from "../config/Database.js";
+
+const {DataTypes} = Sequelize;
+
+const Product = db.define('product',{
+    name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    image: DataTypes.STRING,
+    url: DataTypes.STRING,
+    desc: DataTypes.TEXT
+},{
+    freezeTableName: true
+});
+
+export default Product;
+
+(async()=>{
+    await db.sync();
+})();
